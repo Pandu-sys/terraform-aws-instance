e216f725dc7f1eb9d25f1438c8c7d6715baf16e2
Pandu Rani <ranipandu340@gmail.com>
2026-06-24 10:59:13 +0530

    “terraform”

---
+++ variable.tf
@@ -0,0 +1,32 @@
+# mandatory
+variable "project" {
+
+}
+
+variable "environment" {
+    
+} 
+
+variable "component" {
+    type = string
+}
+
+variable "instance_type" {
+    default = "t3.micro"
+    type = string
+
+    validation {
+    condition     = contains(["t3.micro", "t3.small", "t3.medium"], var.instance_type)
+    error_message = "instance type should be either t3.micro or t3.small"
+  }
+}
+
+variable "sg_ids" {
+    type = list
+}
+
+# optional
+variable "ec2_tags" {
+    default = {}
+    type = map
+}
